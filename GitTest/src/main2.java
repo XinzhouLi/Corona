5d5d2e4bac8c23fb809678beebf4dc652742fa3a
979649882 <xudong.miao1@ucalgary.ca>
2019-07-19 17:24:07 -0600

    Create main2.java

---
+++ GitTest/src/main2.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.Random;
+public class main2 {
+	public static void main(String[] args) {
+		ArrayList<Integer> maplist=new ArrayList();
+		for(int i=0;i<12;i+=1) {
+			maplist.add(0);
+		}
+		
+	}
+	public static int diceNumber() {
+		Random rand=new Random();
+		int diceNumber=rand.nextInt(7);
+		while(diceNumber==0) {
+			diceNumber=rand.nextInt(7);
+		}
+		return diceNumber;
+		}
+	public static void locationUpadate(Player p) {
+		p.Location+=diceNumber();
+	}
+	public static void loseMoney(Player player,Property player1) {
+		ArrayList map=monopolyMap.getMap();
+		if(player.Location!=)
+		if(player.Money>=player1.Cost) {
+				player.Money-=player1.getCost();
+		}
+	}
+}
