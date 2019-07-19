7d6fbaca8225d9398d67af83107c07979f987e72
joseph5-ship <52938437+joseph5-ship@users.noreply.github.com>
2019-07-19 17:19:11 -0600

    Update monopolyMap.java
    map

--- GitTest/src/monopolyMap.java
+++ GitTest/src/monopolyMap.java
@@ -23,14 +23,23 @@ public class monopolyMap {
    	 }
    	 return locationValue;
     }
+
+    /**
+     * Method will pick a random card from a digital pile and award the player with money.
+     */
     public void pickRandomCard1(Player a) {
     	System.out.println("you picked random card 1: Schalorship\n you win $50");
     	a.setMoney(a.getMoney() + 50);
     }
+
+    /**
+     * Method will pick a random card from a digital pile and award the player with money.
+     */
     public void pickRandomCard2(Player a) {
     	System.out.println("you picked random card 2: Fees\n you lose $50");
     	loseMoney(a,50);
-    	}
+        }
+        
     /**
      * Methods pick a random card that will bestow a random effect on the player.
      * @param a Of type Player that holds info on player's card effect.
@@ -51,28 +60,45 @@ public class monopolyMap {
      * @param a Of type Player that holds info on properties under their name.  
      * 
      */
-    public void propertyCardEffects(Player a) {
-        if (propertyCardValue == 0) {
-            a.addProperty("House");
-        } else if (propertyCardValue == 1) {
-            a.addProperty("Hotel");
-        } else {
-            a.addProperty("Hospital");
-        }
+    // public void propertyCardEffects(Player a) {
+    //     if (propertyCardValue == 0) {
+    //         a.addProperty("House");
+    //     } else if (propertyCardValue == 1) {
+    //         a.addProperty("Hotel");
+    //     } else {
+    //         a.addProperty("Hospital");
+    //     }
 
-    }
+    // }
 
+    /**
+     * Method will allow player to purchase property.
+     * @param a Of type Player taht holds info on who owns property and of how much info to code
+     * @param b 
+     */
 	public void propertyCardBuy(Player a, Property b) {
 		b.setWho(a.PlayerName);
 		a.setMoney(a.getMoney() - b.getCost());
 		a.addProperty(b.PropertyName);
 		
-	}
+    }
+    
+    /**
+     * Method will allow player to sell property.
+     */
 	public void propertyCardSell(Property a, Player b) {
 		b.getProperties().remove(a);
 		a.setWho("");
 		b.setMoney(b.getMoney() + a.getCost());
-	}
+    }
+    
+    /**
+     * Method will allow player to transfer their own funds to another 
+     * player.
+     * @param a Type Player that holds info on money
+     * @param c Type Player that holds info on money
+     * @param k Type Player that holds info on money
+     */
 	public void Transfer(Player a, Player c, int k) {
 		a.setMoney(a.getMoney() + k);
 		c.setMoney(c.getMoney() - k);
