ca3738907b93aac87e0ed1a7e79798cc6b258bc9
thefuseman01 <52938430+thefuseman01@users.noreply.github.com>
2019-07-19 13:57:13 -0600

    Create Player.java

---
+++ GitTest/src/Player.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+
+public class Player{
+	int Money;
+	int Location;
+	ArrayList<String> Properties = new ArrayList<String>();
+	
+	public Player(int Money, int Location, ArrayList<String> Properties) {
+		setMoney(Money);
+		setLocation(Location);
+		setProperties(Properties);
+	}
+	
+	 public Player (Player player){
+		 this.Money = player.Money;
+		 this.Location = player.Location;
+		 this.Properties = player.Properties;
+	}
+	
+	public void setMoney(int Money) {
+		this.Money = Money;
+	}
+	
+	public void setLocation(int Location) {
+		this.Location = Location;
+	}
+	
+	public void setProperties(ArrayList<String> Properties) {
+		this.Properties = Properties;
+	}
+	
+	public int getMoney() {
+		return Money;
+	}
+	
+	public int getLocation() {
+		return Location;
+	}
+	
+	public ArrayList<String> getProperties(){
+		return Properties;
+	}
+}
