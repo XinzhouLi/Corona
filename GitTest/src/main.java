bc034317267ae114a4923c297d86747cdcdd0822
thefuseman01 <52938430+thefuseman01@users.noreply.github.com>
2019-07-19 17:14:21 -0600

    Update main.java

--- GitTest/src/main.java
+++ GitTest/src/main.java
@@ -2,7 +2,17 @@
 public class main {
 
 	public static void main(String[] args) {
+		public boolean WinCondition(Player player){ //Incomplete method for checking if a player goes bankrupt
+			while (player.getMoney() <= 0) {
+				if (player.Properties != null) {
+					monopolyMap.propertyCardsell( , player);
+				}
 
+			else {
+				return false;
+			}
+		}
+	}
 	}
 
 }
