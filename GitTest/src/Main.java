63c731e63f29aae6d417a5d243e67654dd55a6a2
lixinzhou9822@163.com <lixinzhou9822@163.com>
2019-07-20 19:30:08 -0600

    Create Main.java

---
+++ GitTest/src/Main.java
@@ -0,0 +1,30 @@
+
+public class Main {
+
+		public static void main(String[] args) {
+			Player p0 = new Player(1500, 0, null, 0);
+			Player p1 = new Player(1500, 0, null, 1);
+			Player p2 = new Player(1500, 0, null, 2);
+			Player p3 = new Player(1500, 0, null, 3);
+			
+			int round = 0;
+			
+			
+//			Starting game
+			while ( p0.getMoney()==0 &&
+						p1.getMoney()==0&&
+						p2.getMoney()==0&&
+						p3.getMoney()==0) {   // start round
+				
+				
+//				Reset round
+				round++;
+				if (round == 4 ) {
+					round = 0;
+				}
+			}
+					
+					
+				
+		}
+}
