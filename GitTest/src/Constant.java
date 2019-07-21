1c132ca9111ca37868163ac984e3fa64a1f9d59b
lixinzhou9822@163.com <lixinzhou9822@163.com>
2019-07-20 19:08:53 -0600

    Update Constant.java

--- GitTest/src/Constant.java
+++ GitTest/src/Constant.java
@@ -1,6 +1,9 @@
-package MyGame;
 
 public class Constant {
-	public static final int GAME_WIDTH = 500;
-	public static final int GAME_HEIGHT = 500;
+	
+	public static final int INITAIAL_MONEY = 1500;
+	public static final int PROPERTY_PRICE = 200;
+	public static final int BUILDING_PRICE = 200;
+	public static final int FINE = 50;
+
 }
