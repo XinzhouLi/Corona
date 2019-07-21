ad1c912e6dcd9c1e59a3c9f4d6c94692c6b20f6b
joseph5-ship <52938437+joseph5-ship@users.noreply.github.com>
2019-07-21 15:57:06 -0600

    Update Player.java

--- GitTest/src/Player.java
+++ GitTest/src/Player.java
@@ -8,6 +8,9 @@ public class Player{
 	private int PlayerNumber;
 	private String PlayerName;
 
+	/**
+	 * Method will set money, location, associated property, player number, and player name.
+	 */
 	public Player(int Money, int Location, ArrayList<String> Properties, int PlayerNumber) { //Constructor
 		setMoney(Money);
 		setLocation(Location);
@@ -16,6 +19,12 @@ public class Player{
 		setPlayerName();
 	}
 	
+	/**
+	 * Method acts as a copy constructor and sets a new instance of player's money, location, 
+	 * properties, player number, and name.
+	 * @param player Of type player and holds info on player's money, location, 
+	 * properties, player number, and name.
+	 */
 	public Player (Player player){ //Copy Constructor
 		 this.Money = player.Money;
 		 this.Location = player.Location;
@@ -23,23 +32,41 @@ public class Player{
 		 this.PlayerNumber = player.PlayerNumber;
 		 this.PlayerName = player.PlayerName;
 	}
-	
+
+	/**
+	 * Method sets player's amount of money.
+	 */
 	public void setMoney(int Money) {
 		this.Money = Money;
 	}
 	
+	/**
+	 * Method sets player's current location as an integer value. 
+	 */
 	public void setLocation(int Location) {
 		this.Location = Location;
 	}
 	
+	/**
+	 * Method will set properties belonging to the player. 
+	 * @param Properties An ArrayList that hold values associated to each player.
+	 */
 	public void setProperties(ArrayList<String> Properties) {
 		this.Properties = Properties;
 	}
 	
+	/**
+	 * Method will set player's name.
+	 * @param PlayerNumber An integer that holds information on player's number.
+	 */
 	public void setPlayerNumber(int PlayerNumber) {
 		this.PlayerNumber = PlayerNumber;
 	}
 	
+	/**
+	 * Method will set player name
+	 * @return
+	 */
 	public String setPlayerName() {
 		Scanner input = new Scanner(System.in);
         String s = input.nextLine();
