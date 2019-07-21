5a5621ee337c5049877671361275576b81ac1e1a
thefuseman01 <52938430+thefuseman01@users.noreply.github.com>
2019-07-21 16:31:11 -0600

    Update Services.java

--- GitTest/src/Services.java
+++ GitTest/src/Services.java
@@ -2,10 +2,6 @@ import java.util.ArrayList;
 import java.util.Random;
 public class Services {
 
-	/**
-	 * Public, static method that rolls a six-sided die. 
-	 * @return diceNumber, or the random value of the dice roll.
-	 */
 	public static int diceNumber() {
 		Random rand=new Random();
 		int diceNumber=rand.nextInt(7);
@@ -15,11 +11,6 @@ public class Services {
 		return diceNumber;
 	}
 	
-	/**
-	 * Public, static method with void return type that 
-	 * updates the current location of the player.
-	 * @param p A variable holding the player's information.
-	 */
 	public static void locationUpdate(Player p) {
 		int location=p.getLocation();
 		location+=diceNumber();
@@ -30,42 +21,21 @@ public class Services {
 
 	}
 	
-	/**
-	 * Public, static method with return type void that gets information
-	 * on the player's location and makes them pay rent if they are on a property board
-	 * space owned by another player. 
-	 * @param player Holds info on who the player is
-	 * @param landOwner Holds info on who owns the property
-	 * @param landInformation Holds info on what kind of property it is
-	 * @param propertyList Holds an array list of information of all player's properties
-	 */
 	public static void rent(Player player,Player landOwner,Property landInformation,ArrayList<Integer> propertyList) {
 		int location=player.getLocation();
 		System.out.println(location);
 		if(propertyList.get(location)!=player.getPlayerNumber()&&location!=6 && player != landOwner) {
-			System.out.println("player:"+player.getPlayerName()+"pay"+landInformation.getRent()+"to"+landOwner.getPlayerName());
+			System.out.println(player.getPlayerName()+"pays "+landInformation.getRent()+" to "+landOwner.getPlayerName());
 			Transfer(player,landOwner,landInformation.getRent());
 			System.out.println("Rent paid!");
 		}
 	}
 	
-	/**
-	 * Transfers one player's funds to another player's bank. 
-	 * @param a Holds information on the player
-	 * @param landOwner Holds information on who owns the land
-	 * @param moneyAmount Hold info on the money amount being transferred,
-	 */
 	public static void Transfer(Player a,Player landOwner, int moneyAmount) {
 		a.setMoney(a.getMoney() + moneyAmount);
 		landOwner.setMoney(landOwner.getMoney() - moneyAmount);
 	}
 	
-	/**
-	 * Public, static
-	 * @param player Holds information on player.
-	 * @param landInformation
-	 * @param propertyList
-	 */
 	public static void payJail(Player player,Property landInformation,ArrayList<Integer> propertyList) {
 		int location=player.getLocation();
 		int moneyRemained=player.getMoney();
