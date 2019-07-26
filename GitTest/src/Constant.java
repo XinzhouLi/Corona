ff4d70cceca9ec87f265a28cb7b0b8529dab23b7
979649882 <xudong.miao1@ucalgary.ca>
2019-07-26 15:19:29 -0600

    Update Constant.java

--- GitTest/src/Constant.java
+++ GitTest/src/Constant.java
@@ -5,5 +5,11 @@ public class Constant {
 	public static final int PROPERTY_PRICE = 200;
 	public static final int BUILDING_PRICE = 200;
 	public static final int FINE = 50;
-
+	public static final int START_POSITION=0;
+	public static final int JAIL=10;
+	public static final int FIRST_RANDOM=5;
+	public static final int SECOND_RANDOM=15;
+	public static final int MINIMUM_LOCATION=0;
+	public static final int MAXIMUM_LOCATION=19;
+	
 }
