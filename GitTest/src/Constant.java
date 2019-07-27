d74933259a69c25bc0f6d5653ea83cd26f0af38a
lixinzhou9822@163.com <lixinzhou9822@163.com>
2019-07-27 15:00:50 -0600

    ...

--- GitTest/src/Constant.java
+++ GitTest/src/Constant.java
@@ -4,6 +4,7 @@ public class Constant {
 	public static final int INITAIAL_MONEY = 1500;
 	public static final int PROPERTY_PRICE = 200;
 	public static final int BUILDING_PRICE = 200;
+	public static final int INCREASING_RENT=150;
 	public static final int FINE = 50;
 	public static final int START_POSITION=0;
 	public static final int JAIL=10;
--- GitTest/src/Gameplay.java
+++ GitTest/src/Gameplay.java
@@ -21,10 +21,10 @@ public class Gameplay {
 	}
 	public static ArrayList<Player> playersList() {
 		System.out.println("Initializing...");
-		Player p0 = new Player(1500, 0, null, 0);
-		Player p1 = new Player(1500, 0, null, 1);
-		Player p2 = new Player(1500, 0, null, 2);
-		Player p3 = new Player(1500, 0, null, 3);
+		Player p0 = new Player(0);
+		Player p1 = new Player(1);
+		Player p2 = new Player(2);
+		Player p3 = new Player(3);
 		ArrayList<Player> playerList =new ArrayList<Player>();
 		playerList.add(p0);
 		playerList.add(p1);
@@ -34,26 +34,26 @@ public class Gameplay {
 	}
 	
 	public static ArrayList<Property> propertiesList() {
-		Property b0 = new Property(0);
-		Property b1 = new Property(1);
-		Property b2 = new Property(2);
-		Property b3 = new Property(3);
-		Property b4 = new Property(4);
-		Property b5 = new Property(5);
-		Property b6 = new Property(6);
-		Property b7 = new Property(7);
-		Property b8 = new Property(8);
-		Property b9 = new Property(9);
-		Property b10 = new Property(10);
-		Property b11 = new Property(11);
-		Property b12 = new Property(12);
-		Property b13 = new Property(13);
-		Property b14 = new Property(14);
-		Property b15 = new Property(15);
-		Property b16 = new Property(16);
-		Property b17 = new Property(17);
-		Property b18 = new Property(18);
-		Property b19 = new Property(19);
+		Property b0 = new Property(0,"Start");
+		Property b1 = new Property(1,"Canada");
+		Property b2 = new Property(2,"Brazil");
+		Property b3 = new Property(3,"Russia");
+		Property b4 = new Property(4, "Mexico");
+		Property b5 = new Property(5,"Random");
+		Property b6 = new Property(6,"Egypt");
+		Property b7 = new Property(7,"Turkey");
+		Property b8 = new Property(8,"Germany");
+		Property b9 = new Property(9,"United Kingdom");
+		Property b10 = new Property(10,"Jail");
+		Property b11 = new Property(11,"Italy");
+		Property b12 = new Property(12,"South Africa");
+		Property b13 = new Property(13,"South Korea");
+		Property b14 = new Property(14,"Colombia");
+		Property b15 = new Property(15,"Radom");
+		Property b16 = new Property(16,"China");
+		Property b17 = new Property(17,"India");
+		Property b18 = new Property(18,"United States");
+		Property b19 = new Property(19,"Japan");
 		ArrayList<Property> propertiesList = new ArrayList<Property>();
 		propertiesList.add(b0);
 		propertiesList.add(b1);
@@ -81,7 +81,7 @@ public class Gameplay {
 	
 	
 	public static void startGame(ArrayList<Player> playersList, ArrayList<Property> propertiesList) {
-		while(winingCondiction(playersList)) {
+		while(Services.winingCondiction(playersList)) {
 			
 				//Start round
 			
@@ -89,32 +89,32 @@ public class Gameplay {
 				System.out.println(currentPlayer.getPlayerName() + "'s turn");
 				System.out.println("Money: " + currentPlayer.getMoney());
 				System.out.println("Location: " + currentPlayer.getLocation());
-				locationUpdate(currentPlayer);
+				Services.locationUpdate(currentPlayer);
 				System.out.println("Location: " + currentPlayer.getLocation());
 				
 				//special location checking
 				
 				if(currentPlayer.getLocation() == 10) {
-					payJail(currentPlayer);
+					Services.payJail(currentPlayer);
 				}else if (currentPlayer.getLocation() == 5||currentPlayer.getLocation() == 15) {
-					UsingRandom.randomCard(playersList,propertiesList);
+					UsingRandomCard.randomCard(propertiesList,currentPlayer);
 				}
 
 				//if it is normal property, buying,renting ,building
 				if (propertiesList.get(currentPlayer.getLocation()).getOwner() == currentPlayer.getPlayerNumber()) {
-					buildHouse(currentPlayer, propertiesList);
+					Services.buildHouse(currentPlayer, propertiesList);
 				}else if (propertiesList.get(currentPlayer.getLocation()).getOwner() == 5) {
-					buyLand(currentPlayer, propertiesList);
+					Services.buyLand(currentPlayer, propertiesList);
 				}else if (propertiesList.get(currentPlayer.getLocation()).getOwner() != currentPlayer.getPlayerNumber()) {
-					rent(currentPlayer, propertiesList, playersList);
+					Services.rent(currentPlayer, propertiesList, playersList);
 				}
 				System.out.println("");
-				if (winingCondiction(playersList)) {
+				if (Services.winingCondiction(playersList)) {
 					break;
 				}
 			}
 		}
-		findWinner(playersList, propertiesList);
+		Services.findWinner(playersList, propertiesList);
 		System.out.println("End Game");
 		
 	}
