bc4347bfa48493256e08a29a51b1fb7e4a2d4879
joseph5-ship <52938437+joseph5-ship@users.noreply.github.com>
2019-07-28 15:02:58 -0600

    Update gui123.java

--- GitTest/src/gui123.java
+++ GitTest/src/gui123.java
@@ -71,15 +71,101 @@ public class gui123{
 		 * @param propertiesList Holds info on the properties and what numebr is at which index 
 		 * that corresponds to a player. 
 		 */
-	public void startGame(ArrayList<Player> playersList, ArrayList<Property> propertiesList) {
-			
+	public void startGame(ArrayList<Player> playersList, ArrayList<Property> propertiesList) {	
 		//setting layout
 		JFrame MonopolyMap = new JFrame ("Monopoly Map");
 		MonopolyMap.setLayout(new GridLayout(6, 6));
 		JTextArea square = new JTextArea(); 
+
+		//initializing buttons for properties
+		JButton b0 = new JButton("GO");  
+	    JButton b1 = new JButton("property 1");  
+	    JButton b2 = new JButton("property 2");  
+	    JButton b3 = new JButton("property 3");  
+	    JButton b4 = new JButton("property 4");  
+	    JButton b5 = new JButton("Random");  
+	    JButton b6 = new JButton("property 6");  
+	    JButton b7 = new JButton("property 7");  
+	    JButton b8 = new JButton("property 8");  
+	    JButton b9 = new JButton("property 9");
+	    JButton b10 = new JButton("Jail");  
+	    JButton b11 = new JButton("property 11");  
+	    JButton b12 = new JButton("property 12");  
+	    JButton b13 = new JButton("property 13");  
+	    JButton b14 = new JButton("property 14");  
+	    JButton b15 = new JButton("Random");  
+	    JButton b16 = new JButton("property 16");  
+	    JButton b17 = new JButton("property 17");  
+	    JButton b18 = new JButton("property 18");  
+	    JButton b19 = new JButton("property 19");  
+		
+		//adding the buttons to a list 
+		buttonToProperty.add(b0);
+		buttonToProperty.add(b1);
+		buttonToProperty.add(b2);
+		buttonToProperty.add(b3);
+		buttonToProperty.add(b4);
+		buttonToProperty.add(b5);
+		buttonToProperty.add(b6);
+		buttonToProperty.add(b7);
+		buttonToProperty.add(b8);
+		buttonToProperty.add(b9);
+		buttonToProperty.add(b10);
+		buttonToProperty.add(b11);
+		buttonToProperty.add(b12);
+		buttonToProperty.add(b13);
+		buttonToProperty.add(b14);
+		buttonToProperty.add(b15);
+		buttonToProperty.add(b16);
+		buttonToProperty.add(b17);
+		buttonToProperty.add(b18);
+		buttonToProperty.add(b19);
+
+		//Adding MonopolyMap spaces 
+		MonopolyMap.add(b0);
+		MonopolyMap.add(b1);
+		MonopolyMap.add(b2);
+		MonopolyMap.add(b3);
+		MonopolyMap.add(b4);
+		MonopolyMap.add(b5);
+		MonopolyMap.add(b19);
+		MonopolyMap.add(new JButton(""));
+		MonopolyMap.add(new JButton(""));
+		MonopolyMap.add(new JButton(""));
+		MonopolyMap.add(new JButton(""));
+		MonopolyMap.add(b6);
+		MonopolyMap.add(b18);
+		MonopolyMap.add(new JButton(""));
+		MonopolyMap.add(new JButton(""));
+		MonopolyMap.add(new JButton(""));
+		MonopolyMap.add(new JButton(""));
+
+		MonopolyMap.add(b7);
+		MonopolyMap.add(b17);
+		MonopolyMap.add(new JButton(""));
+		MonopolyMap.add(new JButton(""));
+		MonopolyMap.add(new JButton(""));
+		MonopolyMap.add(new JButton(""));
+
+		MonopolyMap.add(b8);
+		MonopolyMap.add(b16);
+		MonopolyMap.add(new JButton(""));
+		MonopolyMap.add(new JButton(""));
+		MonopolyMap.add(new JButton(""));
+		MonopolyMap.add(new JButton(""));
+		MonopolyMap.add(b9);
+		MonopolyMap.add(b15);
+		MonopolyMap.add(b14);
+		MonopolyMap.add(b13);
+		MonopolyMap.add(b12);
+		MonopolyMap.add(b11);
+		MonopolyMap.add(b10);
 	}
-	public static void main(String[] args) { }
-		//initialize class startGame
+	public static void main(String[] args) { 
+
+	} 
+
+	
 		
 		
 
@@ -93,85 +179,9 @@ public class gui123{
 //		JButton goButton = new JButton();
 //		goButton.setIcon(new ImageIcon("C:\\Users\\User\\Desktop\\CPSC233\\ProjectImages\\Go.png"));
 ////		MonopolyMap.add(goButton);
-	    JButton b0=new JButton("GO");  
-	    JButton b1=new JButton("property 1");  
-	    JButton b2=new JButton("property 2");  
-	    JButton b3=new JButton("property 3");  
-	    JButton b4=new JButton("property 4");  
-	    JButton b5=new JButton("Random");  
-	    JButton b6=new JButton("property 6");  
-	    JButton b7=new JButton("property 7");  
-	    JButton b8=new JButton("property 8");  
-	    JButton b9=new JButton("property 9");
-	    JButton b10=new JButton("Jail");  
-	    JButton b11=new JButton("property 11");  
-	    JButton b12=new JButton("property 12");  
-	    JButton b13=new JButton("property 13");  
-	    JButton b14=new JButton("property 14");  
-	    JButton b15=new JButton("Random");  
-	    JButton b16=new JButton("property 16");  
-	    JButton b17=new JButton("property 17");  
-	    JButton b18=new JButton("property 18");  
-	    JButton b19=new JButton("property 19");  
-		buttontoProperty.add(b0);
-	buttontoProperty.add(b1);
-	buttontoProperty.add(b2);
-	buttontoProperty.add(b3);
-	buttontoProperty.add(b4);
-	buttontoProperty.add(b5);
-	buttontoProperty.add(b6);
-	buttontoProperty.add(b7);
-	buttontoProperty.add(b8);
-	buttontoProperty.add(b9);
-	buttontoProperty.add(b10);
-	buttontoProperty.add(b11);
-	buttontoProperty.add(b12);
-	buttontoProperty.add(b13);
-	buttontoProperty.add(b14);
-	buttontoProperty.add(b15);
-	buttontoProperty.add(b16);
-	buttontoProperty.add(b17);
-	buttontoProperty.add(b18);
-	buttontoProperty.add(b19);
 	    
-	MonopolyMap.add(b0);
-	MonopolyMap.add(b1);
-	MonopolyMap.add(b2);
-	MonopolyMap.add(b3);
-	MonopolyMap.add(b4);
-	MonopolyMap.add(b5);
-	MonopolyMap.add(b19);
-	MonopolyMap.add(new JButton(""));
-	MonopolyMap.add(new JButton(""));
-	MonopolyMap.add(new JButton(""));
-	MonopolyMap.add(new JButton(""));
-	MonopolyMap.add(b6);
-	MonopolyMap.add(b18);
-	MonopolyMap.add(new JButton(""));
-	MonopolyMap.add(new JButton(""));
-	MonopolyMap.add(new JButton(""));
-	MonopolyMap.add(new JButton(""));
-
-	MonopolyMap.add(b7);
-	MonopolyMap.add(b17);
-	MonopolyMap.add(new JButton(""));
-	MonopolyMap.add(new JButton(""));
-	MonopolyMap.add(new JButton(""));
-	MonopolyMap.add(new JButton(""));
-
-	MonopolyMap.add(b8);
-	MonopolyMap.add(b16);
-	MonopolyMap.add(new JButton(""));
-	MonopolyMap.add(new JButton(""));
-	MonopolyMap.add(new JButton(""));
-	MonopolyMap.add(new JButton(""));
-	MonopolyMap.add(b9);
-	MonopolyMap.add(b15);
-	MonopolyMap.add(b14);
-	MonopolyMap.add(b13);
-	MonopolyMap.add(b12);
-	MonopolyMap.add(b11);
-	MonopolyMap.add(b10);
+	    
+	
 
 	
 
