fcf50ad4576187007ce68bdcc1092a1e0fd7adea
979649882 <xudong.miao1@ucalgary.ca>
2019-08-05 13:46:01 -0600

    create cp

--- GitTest/src/createCP.java
+++ GitTest/src/createCP.java
@@ -1,14 +1,9 @@
 import java.util.Scanner;
 
 public class createCP {
-	public static Player createComputerPlayer(Player player) {
+	public static Player createComputerPlayer(Player player,int diffi) {
 		Player a= player;
-		System.out.println("Choose the level of difficulty for the computer player");
-		int diffi=new Scanner(System.in).nextInt();
-		while(diffi<1&&diffi>2) {
-			System.out.println("Choose the level of difficulty for the computer player");
-			diffi=new Scanner(System.in).nextInt();
-		}
+
 		if(diffi==1) {
 			a=new cpdifficulty1(player);
 		}
